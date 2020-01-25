bba370a1fdae86749f76ea6dccfd07044c7ebab7
Ayfri <pierre.ayfri@gmail.com>
2020-01-25 18:02:29 +0100

    Change of require paths for a relative path
    This is a shorter way to require files.

--- commands/tests/help.js
+++ commands/tests/help.js
@@ -1,15 +1,16 @@
-const Command = require('../../classes/Command.js');
-const getThing = require('../../utils/getThing.js');
-const BetterEmbed = require('../../classes/BetterEmbeds.js');
+const Command = require('classes/Command.js');
+const getThing = require('utils/getThing.js');
+const BetterEmbed = require('utils/BetterEmbeds.js');
 
 module.exports = new Command({
-	name: 'help',
+	name       : 'help',
 	description: 'A simple help command.',
-	usage: 'help <command>',
-	aliases: ['h']
+	usage      : 'help <command>',
+	aliases    : ['h']
 }, async (client, message, args) => {
 	
 	const embed = new BetterEmbed();
+	let command;
 	if (command = await getThing('command', args[0])) {
 		
 		embed.title = `Help on command : ${command.name}`;
@@ -19,10 +20,31 @@ module.exports = new Command({
 			value: command.description
 		});
 		
-		if (command.usage !== undefined) embed.addField('Syntax :', command.usage);
-		if (command.userPermissions !== undefined) embed.addField('User permissions required :', command.userPermissions.join(' '));
-		if (command.clientPermissions !== undefined) embed.addField('Bot permissions required :', command.clientPermissions.join(' '));
-		if (command.aliases !== undefined) embed.addField('Aliases :', command.aliases.join(' '));
+		if (command.usage !== undefined) {
+			embed.fields.push({
+				name : 'Syntax :',
+				value: command.usage
+			});
+		}
+		if (command.userPermissions !== undefined) {
+			embed.fields.push({
+				name : 'User permissions required :',
+				value: command.userPermissions.join(' ')
+			});
+		}
+		if (command.clientPermissions !== undefined) {
+			embed.fields.push({
+				name : 'Bot permissions required :',
+				value: command.clientPermissions.join(' ')
+			});
+		}
+		if (command.aliases !== undefined) {
+			embed.fields.push({
+				name : 'Aliases :',
+				value:
+					command.aliases.join(' ')
+			});
+		}
 		
 	} else {
 		embed.title = 'Here is the list of commands:';
