41dffdaac4796024eae47e9545b9da435e3bc6ca
Ayfri <pierre.ayfri@gmail.com>
2020-04-13 02:25:52 +0200

    Made that the error is thrown

--- utils/argError.js
+++ utils/argError.js
@@ -30,7 +30,7 @@ module.exports = (channel, error, command) => {
 		if (client.channels.get(channel)) {
 			channel = client.channels.get(channel);
 		} else {
-			new Error(`The channel ${channel} is not valid, an ID is expected, if an ID has been entered, then the bot does not have this channel.`);
+			 Error(`The channel ${channel} is not valid, an ID is expected, if an ID has been entered, then the bot does not have this channel.`);
 		}
 	}
 	
