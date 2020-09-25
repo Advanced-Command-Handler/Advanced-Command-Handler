f496b63f8a34ef13b7846ae47f7de6893f136b2a
Ayfri <pierre.ayfri@gmail.com>
2020-09-25 19:06:30 +0200

    Created tsconfig.json, converted index.js, testing in purpose.

---
+++ index.ts
@@ -0,0 +1,17 @@
+import BetterEmbed from './utils/BetterEmbed.js';
+import Command from './classes/Command.js';
+import CommandHandler from './classes/CommandHandler.js';
+import CommandHandlerError from './classes/CommandHandlerError.js';
+import Logger from './utils/Logger.js';
+import argError from './utils/argError.js';
+import getThing from './utils/getThing.js';
+
+export default {
+	BetterEmbed,
+	Command,
+	CommandHandler,
+	CommandHandlerError,
+	Logger,
+	getThing,
+	argError,
+};
