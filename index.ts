cbf6d3e55b751d40a657c550129220220f6e6125
Ayfri <pierre.ayfri@gmail.com>
2020-12-15 00:43:58 +0100

    Removed exporting BetterEmbed from index.ts.

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
-import BetterEmbed from './src/utils/BetterEmbed.js';
 import Command from './src/classes/Command.js';
 import CommandHandler from './src/classes/CommandHandler.js';
 import CommandHandlerError from './src/classes/CommandHandlerError.js';
-import Logger from './src/utils/Logger.js';
+import {Logger} from './src/utils/Logger.js';
 import argError from './src/utils/argError.js';
 import getThing from './src/utils/getThing.js';
 
 export default {
-	BetterEmbed,
 	Command,
 	CommandHandler,
 	CommandHandlerError,
