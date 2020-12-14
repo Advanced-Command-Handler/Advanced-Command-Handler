e6da55afb38bfe9d894b6119142a5a1086e50b49
Ayfri <pierre.ayfri@gmail.com>
2020-12-14 21:29:29 +0100

    Converted to ts argError, now using BetterEmbed.

---
+++ utils/argError.ts
@@ -0,0 +1,23 @@
+import { Message } from "discord.js";
+import {BetterEmbed} from 'discord.js-better-embed';
+
+/**
+ * Send embed who explains why user failed an argument of the Command.
+ * @param {module:"discord.js".Message} message - Channel where embed will be sent.
+ * @param {string} error - The mistake user made.
+ * @param {Command} command - Which command failed.
+ * @returns {void}
+ */
+module.exports = (message: Message, error: string, command: Command) => {
+	const embed = BetterEmbed.fromTemplate('title', {
+		client: message.client,
+		color: 0xee2200,
+		title: 'Argument error :',
+		description: error
+	})
+
+	if (command.usage) embed.addField('Syntax :', command.usage);
+	
+	if (message.client.channels.cache.has(message.channel.id)) message.channel.send({embed});
+	else throw new Error(`The channel ${message} is not valid, an ID is expected, if an ID has been entered, then the bot does not have this channel.`);
+};
