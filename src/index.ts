6e199ebeb1ce6473c75de390d8acc669df3c2849
Ayfri <pierre.ayfri@gmail.com>
2020-12-15 00:45:05 +0100

    Moved index.ts to src.

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Command from './classes/Command.js';
+import CommandHandler from './classes/CommandHandler.js';
+import CommandHandlerError from './classes/CommandHandlerError.js';
+import {Logger} from './utils/Logger.js';
+import argError from './utils/argError.js';
+import getThing from './utils/getThing.js';
+
+export default {
+	Command,
+	CommandHandler,
+	CommandHandlerError,
+	Logger,
+	getThing,
+	argError,
+};
