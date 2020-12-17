f468c499b40ceb79234047d685e8396251d17a99
Ayfri <pierre.ayfri@gmail.com>
2020-12-17 21:48:35 +0100

    Fixed getThing import in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import CommandHandler from './classes/CommandHandler.js';
 import CommandHandlerError from './classes/CommandHandlerError.js';
 import {Logger} from './utils/Logger.js';
 import argError from './utils/argError.js';
-import getThing from './utils/getThing.js';
+import {getThing} from './utils/getThing.js';
 
 export {Command, CommandHandler, CommandHandlerError, Logger, getThing, argError};
