875e80a64ccb058ee15b68dea18180bb2de025cb
Ayfri <pierre.ayfri@gmail.com>
2020-12-18 13:04:30 +0100

    Added missing exports.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Command from './classes/Command.js';
 import CommandHandler from './classes/CommandHandler.js';
 import CommandHandlerError from './classes/CommandHandlerError.js';
-import {Logger} from './utils/Logger.js';
+import {Logger, colors, LogType, ColorResolvable} from './utils/Logger.js';
 import argError from './utils/argError.js';
-import {getThing} from './utils/getThing.js';
+import {getThing, DataType} from './utils/getThing.js';
 
-export {Command, CommandHandler, CommandHandlerError, Logger, getThing, argError};
+export {Command, CommandHandler, CommandHandlerError, Logger, colors, LogType, ColorResolvable, argError, getThing, DataType};
