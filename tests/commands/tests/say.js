1f17392937e334b58a5782be812e3e7ca89bb735
Ayfri <pierre.ayfri@gmail.com>
2020-12-28 23:57:03 +0100

    Added say command to tests.

---
+++ tests/commands/tests/say.js
@@ -0,0 +1,8 @@
+const {Command, Tag} = require('advanced-command-hander');
+module.exports = new Command({
+	name: 'say',
+	tags: [Tag.guildOnly],
+	userPermissions: ['MANAGE_MESSAGES'],
+}, async (handler, message, args) => {
+	await message.channel.send(args.join(' '));
+})
