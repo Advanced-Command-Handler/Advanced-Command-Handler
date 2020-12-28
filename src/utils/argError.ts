bd691989e4acc6f243970786e56d36274a6c8f45
Ayfri <pierre.ayfri@gmail.com>
2020-12-29 00:20:04 +0100

    Fixed BetterEmbed template in argError.

--- src/utils/argError.ts
+++ src/utils/argError.ts
@@ -3,7 +3,7 @@ import {BetterEmbed} from 'discord.js-better-embed';
 import {Command} from '../classes/Command';
 
 export default (message: Message, error: string, command: Command): Promise<Message> => {
-	const embed = BetterEmbed.fromTemplate('title', {
+	const embed = BetterEmbed.fromTemplate('complete', {
 		client: message.client,
 		color: 0xee2200,
 		title: 'Argument error :',
