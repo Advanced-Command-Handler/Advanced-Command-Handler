c6f0d16e908236eceab3183217823fdae0381fd0
Ayfri <pierre.ayfri@gmail.com>
2020-12-28 20:59:36 +0100

    Added export for permissionsError.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import CommandHandler from './classes/CommandHandler.js';
 import CommandHandlerError from './classes/CommandHandlerError.js';
 import argError from './utils/argError.js';
 import dayjs from 'dayjs';
+import permissionsError from './utils/permissionsError.js';
 
-export {CommandHandler, CommandHandlerError, argError, dayjs};
+export {CommandHandler, CommandHandlerError, argError, dayjs, permissionsError};
 export * from './classes/Command.js';
 export * from './utils/Logger';
 export * from './utils/getThing';
