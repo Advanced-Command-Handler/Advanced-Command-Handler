739384449cf2c92527f61795d1e5f36b2cbb6282
Ayfri <pierre.ayfri@gmail.com>
2020-12-28 20:59:17 +0100

    Added export for dayjs.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import CommandHandler from './classes/CommandHandler.js';
 import CommandHandlerError from './classes/CommandHandlerError.js';
 import argError from './utils/argError.js';
+import dayjs from 'dayjs';
 
-export {CommandHandler, CommandHandlerError, argError};
+export {CommandHandler, CommandHandlerError, argError, dayjs};
 export * from './classes/Command.js';
 export * from './utils/Logger';
 export * from './utils/getThing';
