ae72968a0fe819018beeff6738ec09698f5f0767
Ayfri <pierre.ayfri@gmail.com>
2020-12-29 03:59:59 +0100

    Fixed milliseconds for the Logger date.

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -80,7 +80,7 @@ export class Logger {
 		text = text.replace(/\u001b\[\u001b\[33m39\u001b\[39mm/gi, chalk.reset());
 
 		color = Logger.propertyInEnum(LogType, color) ?? color;
-		text = `${Logger.setColor('#847270', `[${dayjs().format('D HH:mm:ss.u')}]`)}${Logger.setColor(color, `[${title.toUpperCase()}] ${text + chalk.reset()}`)}`;
+		text = `${Logger.setColor('#847270', `[${dayjs().format('D HH:mm:ss.SSS')}]`)}${Logger.setColor(color, `[${title.toUpperCase()}] ${text + chalk.reset()}`)}`;
 		console.log(text);
 	}
 
