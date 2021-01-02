8f7de31f93400e29a11a1dffc137e057f50f048b
Ayfri <pierre.ayfri@gmail.com>
2021-01-02 03:34:00 +0100

    Added run function to Event class.

--- src/classes/Event.ts
+++ src/classes/Event.ts
@@ -1,3 +1,5 @@
+import {RunFunction} from '../types.js';
+
 interface EventsOptions {
 	readonly name: string;
 	once?: boolean;
@@ -6,8 +8,10 @@ interface EventsOptions {
 export default class Event implements EventsOptions {
 	public readonly name: string;
 	public once: boolean;
+	public run: RunFunction;
 
-	public constructor(options: EventsOptions) {
+	public constructor(options: EventsOptions, runFunction: RunFunction) {
+		this.run = runFunction;
 		this.name = options.name;
 		this.once = options.once ?? false;
 	}
