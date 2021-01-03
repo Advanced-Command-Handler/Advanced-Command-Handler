2e71b6224441b3d2a8487a884baa680e85a461e6
Ayfri <pierre.ayfri@gmail.com>
2021-01-04 00:21:19 +0100

    Now using a namespace CommandHandler.

--- src/classes/CommandHandler.ts
+++ src/classes/CommandHandler.ts
@@ -1,109 +1,102 @@
 import {ClientOptions, Collection, Message} from 'discord.js';
-import {EventEmitter} from 'events';
 import {promises as fsPromises} from 'fs';
 import {join} from 'path';
-import {Logger} from '../utils/Logger';
 import AdvancedClient from './AdvancedClient';
 import {Command} from './Command';
 import CommandHandlerError from './CommandHandlerError';
 import Event from './Event';
+import {EventEmitter} from 'events';
+import {Logger} from '../utils/Logger';
 
-export interface CreateCommandHandlerOptions {
-	commandsDir: string;
-	eventsDir: string;
-	owners?: string[];
-	prefixes?: string[];
-}
 
-export interface CommandHandlerInstance extends CreateCommandHandlerOptions {
-	client: AdvancedClient | null;
-	commands: Collection<string, Command>;
-	cooldowns: Collection<string, number>;
-}
-
-type CommandHandlerEvents = {
-	create: [CreateCommandHandlerOptions];
-	error: [CommandHandlerError];
-	launch: [];
-	loadCommand: [Command];
-	loadEvent: [Event];
-	launched: [CommandHandlerInstance];
-};
-
-export default class CommandHandler implements CommandHandlerInstance {
-	public static instance: CommandHandler;
-	public static version: string = require('../../package.json').version;
-	private static emitter: EventEmitter = new EventEmitter();
-	public commandsDir: string;
-	public eventsDir: string;
-	public owners?: string[];
-	public prefixes?: string[];
-	public client: AdvancedClient | null;
-	public commands: Collection<string, Command>;
-	public cooldowns: Collection<string, number>;
-
-	private constructor(options: CreateCommandHandlerOptions) {
-		this.commandsDir = options.commandsDir;
-		this.eventsDir = options.eventsDir;
-		this.owners = options.owners;
-		this.prefixes = options.prefixes;
-		this.client = null;
-		this.cooldowns = new Collection();
-		this.commands = new Collection();
+export namespace CommandHandler {
+	export interface CreateCommandHandlerOptions {
+		commandsDir: string;
+		eventsDir: string;
+		owners?: string[];
+		prefixes?: string[];
 	}
 
-	public static on<K extends keyof CommandHandlerEvents>(eventName: K, fn: (listener: CommandHandlerEvents[K]) => void): void {
-		CommandHandler.emitter.on(eventName, fn);
+	export interface CommandHandlerInstance extends CreateCommandHandlerOptions {
+		client: AdvancedClient | null;
+		commands: Collection<string, Command>;
+		cooldowns: Collection<string, number>;
 	}
 
-	public static create(options: CreateCommandHandlerOptions): typeof CommandHandler {
-		Logger.log(`Advanced Command Handler ${CommandHandler.version} by Ayfri.`, 'Loading', 'red');
-		if (!CommandHandler.instance) CommandHandler.instance = new CommandHandler(options);
+	type CommandHandlerEvents = {
+		create: [CreateCommandHandlerOptions];
+		error: [CommandHandlerError];
+		launch: [];
+		loadCommand: [Command];
+		loadEvent: [Event];
+		launched: [CommandHandlerInstance];
+	};
+
+	export const version: string = require('../../package.json').version;
+	export const emitter: EventEmitter = new EventEmitter();
+	export let commandsDir: string;
+	export let eventsDir: string;
+	export let owners: string[] | undefined;
+	export let prefixes: string[] | undefined;
+	export let client: AdvancedClient | null;
+	export let commands: Collection<string, Command>;
+	export let cooldowns: Collection<string, number>;
+
+	export function on<K extends keyof CommandHandlerEvents>(eventName: K, fn: (listener: CommandHandlerEvents[K]) => void): void {
+		emitter.on(eventName, fn);
+	}
+
+	export function emit<K extends keyof CommandHandlerEvents>(eventName: K, ...args: CommandHandlerEvents[K]) {
+		emitter.emit(eventName, args);
+	}
+
+	export function create(options: CreateCommandHandlerOptions): typeof CommandHandler {
+		Logger.log(`Advanced Command Handler ${version} by Ayfri.`, 'Loading', 'red');
 
 		process.on('warning', error => Logger.error(`An error occurred. \n${error.stack}`));
 		process.on('uncaughtException', error => Logger.error(`An error occurred. \n${error.stack}`));
-		CommandHandler.emit('create', options);
+		emit('create', options);
 		return CommandHandler;
 	}
 
-	public static getPrefixFromMessage(message: Message): string | null {
+	export async function launch(options: {token: string; clientOptions?: ClientOptions}): Promise<CommandHandlerInstance> {
+		client = new AdvancedClient(CommandHandler, options.token, options.clientOptions ?? {});
+		emit('launch');
+
+		try {
+			await loadCommands(commandsDir ?? '');
+			await loadEvents(eventsDir ?? '');
+		} catch (e) {
+			Logger.error(e.stack, 'Loading');
+		}
+
+		await client.login(options.token);
+		prefixes?.push(`<@${client?.user?.id}>`);
+		owners?.push((await client.fetchApplication()).owner?.id ?? '');
+		emit('launched', CommandHandler);
+		return CommandHandler;
+	}
+
+	export function getPrefixFromMessage(message: Message): string | null {
 		let prefix = null;
-		for (const thisPrefix of CommandHandler.instance.prefixes ?? []) {
+		for (const thisPrefix of prefixes ?? []) {
 			if (message.content.startsWith(thisPrefix)) prefix = thisPrefix;
 		}
 
 		return prefix;
 	}
 
-	public static async launch(options: {token: string; clientOptions?: ClientOptions}): Promise<CommandHandlerInstance> {
-		CommandHandler.instance.client = new AdvancedClient(CommandHandler.instance, options.token, options.clientOptions ?? {});
-		CommandHandler.emit('launch');
-
-		try {
-			await CommandHandler.loadCommands(CommandHandler.instance.commandsDir ?? '');
-			await CommandHandler.loadEvents(CommandHandler.instance.eventsDir ?? '');
-		} catch (e) {
-			Logger.error(e.stack, 'Loading');
-		}
-
-		await CommandHandler.instance.client.login(options.token);
-		CommandHandler.instance.prefixes?.push(`<@${CommandHandler.instance.client?.user?.id}>`);
-		CommandHandler.instance.owners?.push((await CommandHandler.instance.client.fetchApplication()).owner?.id ?? '');
-		CommandHandler.emit('launched', CommandHandler.instance);
-		return CommandHandler.instance;
-	}
-
-	public static async loadCommand(path: string, name: string) {
+	export async function loadCommand(path: string, name: string) {
 		let command = await import(join(process.cwd(), `./${path}/${name}`));
 		if (command.default) command = command.default;
 		if (!command) throw new Error(`Command given name or path is not valid.\nPath : ${path}\nName:${name}`);
 		if (command.category === 'None') command.category = path.split(/[\\/]/).pop();
-		CommandHandler.instance.commands.set(name, command);
-		CommandHandler.emit('loadCommand', command);
+		commands.set(name, command);
+		emit('loadCommand', command);
 		Logger.comment(`Loading the command : ${Logger.setColor('gold', name)}`, 'loading');
 	}
 
-	public static async loadCommands(path: string) {
+	export async function loadCommands(path: string) {
 		if (!path) return;
 		const dirs = await fsPromises.readdir(path);
 		Logger.info('Loading commands.', 'loading');
@@ -115,14 +108,14 @@ export default class CommandHandler implements CommandHandlerInstance {
 				Logger.comment(`Commands in the category '${dir}' : (${files.length})`, 'loading');
 
 				for (const file of files) {
-					await CommandHandler.loadCommand(`${path}/${dir}`, file);
+					await loadCommand(`${path}/${dir}`, file);
 				}
 			}
 		}
-		Logger.info(`${CommandHandler.instance.commands.size} commands loaded.`, 'loading');
+		Logger.info(`${commands.size} commands loaded.`, 'loading');
 	}
 
-	public static async loadEvents(path: string) {
+	export async function loadEvents(path: string) {
 		if (!path) return;
 		const files = await fsPromises.readdir(path);
 		Logger.info('Loading events.', 'loading');
@@ -134,18 +127,14 @@ export default class CommandHandler implements CommandHandlerInstance {
 				if ('default' in event && Object.keys(event).length === 1) event = event.default;
 				if (!event) throw new Error(`Command given name or path is not valid.\nPath : ${path}\nName:${file}`);
 
-				if (event.once) CommandHandler.instance.client?.once(event.name, event.run.bind(null, CommandHandler.instance));
-				else CommandHandler.instance.client?.on(event.name, event.run.bind(null, CommandHandler.instance));
+				if (event.once) client?.once(event.name, event.run.bind(null, CommandHandler));
+				else client?.on(event.name, event.run.bind(null, CommandHandler));
 
 				Logger.comment(`Event loading : ${Logger.setColor('gold', `${file.split('.')[0]}.js`)}`, 'loading');
-				CommandHandler.emit('loadEvent', event);
+				emit('loadEvent', event);
 			}
 		}
 
-		Logger.info(`${CommandHandler.instance.client?.eventNames().length ?? 0} events loaded.`, 'loading');
-	}
-
-	public static emit<K extends keyof CommandHandlerEvents>(eventName: K, ...args: CommandHandlerEvents[K]) {
-		CommandHandler.emitter.emit(eventName, args);
+		Logger.info(`${client?.eventNames().length ?? 0} events loaded.`, 'loading');
 	}
 }
