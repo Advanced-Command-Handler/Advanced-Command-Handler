0866848482e7cffc87db6dae086a472a499e2821
Ayfri <pierre.ayfri@gmail.com>
2021-01-13 13:19:04 +0100

    Improved consistency in command handler loading logs.

--- src/classes/CommandHandler.ts
+++ src/classes/CommandHandler.ts
@@ -47,12 +47,12 @@ namespace CommandHandler {
 	}
 
 	export function setDefaultEvents(): typeof CommandHandler {
-		Logger.info('Loading default events.', 'loading');
+		Logger.info('Loading default events.', 'Loading');
 		for (let event of Object.values(defaultEvents)) {
 			events.set(event.default.name, event.default);
-			Logger.comment(`Default ${Logger.setColor('green', event.default.name) + Logger.setColor('comment', ' event loaded.')}`, 'loading');
+			Logger.comment(`Default ${Logger.setColor('green', event.default.name) + Logger.setColor('comment', ' event loaded.')}`, 'Loading');
 		}
-		Logger.info(`Default events loaded. (${Object.values(defaultEvents).length})`, 'loading');
+		Logger.info(`Default events loaded. (${Object.values(defaultEvents).length})`, 'Loading');
 
 		return CommandHandler;
 	}
@@ -61,9 +61,9 @@ namespace CommandHandler {
 		Logger.info('Loading default commands.', 'Loading');
 		for (let command of Object.values(defaultCommands)) {
 			commands.set(command.default.name, command.default);
-			Logger.comment(`Default ${Logger.setColor('green', command.default.name) + Logger.setColor('comment', ' command loaded.')}`, 'loading');
+			Logger.comment(`Default ${Logger.setColor('green', command.default.name) + Logger.setColor('comment', ' command loaded.')}`, 'Loading');
 		}
-		Logger.info(`Default commands loaded. (${Object.keys(defaultCommands)})`, 'loading');
+		Logger.info(`Default commands loaded. (${Object.keys(defaultCommands)})`, 'Loading');
 
 		return CommandHandler;
 	}
@@ -89,12 +89,12 @@ namespace CommandHandler {
 			await loadCommands(commandsDir ?? '');
 			await loadEvents(eventsDir ?? '');
 
-			Logger.comment('Binding events to client.', 'binding');
+			Logger.comment('Binding events to client.', 'Binding');
 			events.forEach(event => {
 				event.bind(client!!);
 			});
 
-			Logger.info(`${client?.eventNames().length ?? 0} events loaded & bind.`, 'loading');
+			Logger.info(`${client?.eventNames().length ?? 0} events loaded & bind.`, 'Loading');
 		} catch (e) {
 			Logger.error(e.stack, 'Loading');
 		}
@@ -119,9 +119,9 @@ namespace CommandHandler {
 
 		const invalidPermissions = command.getInvalidPermissions();
 		if (invalidPermissions.client.length > 0)
-			throw new CommandHandlerError(`Invalid client permissions for '${command.name}' command.\nInvalid Permissions: '${invalidPermissions.client.sort().join(',')}'`, 'LoadingCommands');
+			throw new CommandHandlerError(`Invalid client permissions for '${command.name}' command.\nInvalid Permissions: '${invalidPermissions.client.sort().join(',')}'`, 'Loading');
 		if (invalidPermissions.user.length > 0)
-			throw new CommandHandlerError(`Invalid user permissions for '${command.name}' command.\nInvalid Permissions: '${invalidPermissions.user.sort().join(',')}'`, 'LoadingCommands');
+			throw new CommandHandlerError(`Invalid user permissions for '${command.name}' command.\nInvalid Permissions: '${invalidPermissions.user.sort().join(',')}'`, 'Loading');
 
 		commands.set(command.name, command);
 		emit('loadCommand', command);
@@ -131,27 +131,27 @@ namespace CommandHandler {
 	export async function loadCommands(path: string) {
 		if (!path) return;
 		const dirs = await fsPromises.readdir(path);
-		Logger.info('Loading commands.', 'loading');
-		Logger.comment(`Categories : (${dirs.length})`, 'loading');
+		Logger.info('Loading commands.', 'Loading');
+		Logger.comment(`Categories : (${dirs.length})`, 'Loading');
 		if (dirs) {
 			for (const dir of dirs) {
 				const files = await fsPromises.readdir(join(process.cwd(), `${path}/${dir}`));
 				if (files.length === 0) continue;
-				Logger.comment(`Commands in the category '${dir}' : (${files.length})`, 'loading');
+				Logger.comment(`Commands in the category '${dir}' : (${files.length})`, 'Loading');
 
 				for (const file of files) {
 					await loadCommand(`${path}/${dir}`, file);
 				}
 			}
 		}
-		Logger.info(`${commands.size} commands loaded.`, 'loading');
+		Logger.info(`${commands.size} commands loaded.`, 'Loading');
 	}
 
 	export async function loadEvents(path: string) {
 		if (!path) return;
 		const files = await fsPromises.readdir(path);
-		Logger.info('Loading events.', 'loading');
-		Logger.comment(`Events : (${files.length})`, 'loading');
+		Logger.info('Loading events.', 'Loading');
+		Logger.comment(`Events : (${files.length})`, 'Loading');
 
 		if (files) {
 			for (const file of files) {
@@ -160,7 +160,7 @@ namespace CommandHandler {
 				if (!event) throw new Error(`Command given name or path is not valid.\nPath : ${path}\nName:${file}`);
 				events.set(event.name, event);
 
-				Logger.comment(`Event ${event.name} loading : ${Logger.setColor('gold', `${file.split('.')[0]}.js`)}`, 'loading');
+				Logger.comment(`Event ${event.name} loading : ${Logger.setColor('gold', `${file.split('.')[0]}.js`)}`, 'Loading');
 			}
 		}
 	}
