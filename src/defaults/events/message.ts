704a45c3a157333eae62f42da45e1b80362c5de7
Ayfri <pierre.ayfri@gmail.com>
2021-01-17 04:33:04 +0100

    Added cooldown handling in default message event.

--- src/defaults/events/message.ts
+++ src/defaults/events/message.ts
@@ -1,15 +1,17 @@
-import {Message} from 'discord.js';
-import {Tag} from '../../classes/Command';
+import {Collection, Message} from 'discord.js';
+import {Command, Tag} from '../../classes/Command';
 import CommandHandler from '../../classes/CommandHandler';
 import Event from '../../classes/Event';
 import argError from '../../utils/argError';
 import {getThing} from '../../utils/getThing';
 import {Logger} from '../../utils/Logger';
 import permissionsError from '../../utils/permissionsError';
+import CooldownUser = CommandHandler.CooldownUser;
+import CommandCooldown = CommandHandler.CommandCooldown;
 
 export default new Event(
 	{
-		name: 'message',
+		name: 'message'
 	},
 	async (handler: typeof CommandHandler, message: Message): Promise<any> => {
 		if (message.author.bot || message.system) return;
@@ -17,15 +19,17 @@ export default new Event(
 		const prefix = CommandHandler.getPrefixFromMessage(message);
 		if (!prefix) return;
 		const args = message.content.slice(prefix.length).trim().split(/ +/g);
-		const cmd = await getThing('command', args[0].toLowerCase().normalize());
+		const command = await getThing('command', args[0].toLowerCase().normalize());
 		args.shift();
 
-		if (cmd && cmd.isInRightChannel(message)) {
-			const missingPermissions = cmd.getMissingPermissions(message);
-			const missingTags = cmd.getMissingTags(message);
 
-			if (missingPermissions.client.length) return permissionsError(message, missingPermissions.client, cmd, true);
-			if (missingPermissions.user.length) return permissionsError(message, missingPermissions.user, cmd);
+		if (command?.isInCooldown(message)) return message.channel.send(`You are in cooldown, please wait **${command?.getCooldown(message).waitMore / 1000}**s.`);
+		if (command && command.isInRightChannel(message)) {
+			const missingPermissions = command.getMissingPermissions(message);
+			const missingTags = command.getMissingTags(message);
+
+			if (missingPermissions.client.length) return permissionsError(message, missingPermissions.client, command, true);
+			if (missingPermissions.user.length) return permissionsError(message, missingPermissions.user, command);
 
 			if (missingTags.length)
 				return argError(
@@ -35,11 +39,12 @@ export default new Event(
 						.sort()
 						.join('\n')
 						.toUpperCase()}\``,
-					cmd
+					command
 				);
 			try {
-				await cmd.run(handler, message, args);
-				Logger.log(`${message.author.tag} has executed the command ${Logger.setColor('red', cmd.name)}.`);
+				await command.run(handler, message, args);
+				command.setCooldown(message);
+				Logger.log(`${message.author.tag} has executed the command ${Logger.setColor('red', command.name)}.`);
 			} catch (error) {
 				Logger.warn(error.stack);
 			}
