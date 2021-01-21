5834356b319d2bcad850d7ee896ce4e753f3f029
Ayfri <pierre.ayfri@gmail.com>
2021-01-22 00:47:28 +0100

    Exported utils functions.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,17 @@
-function random<T extends any>(array: T[]): T {
-	return array[Math.floor(Math.random() * array.length)];
+function cutIfTooLong(text: string, maxLength: number, endTextIfTooLong: string = '...'): string {
+	return text.length > maxLength ? `${text.substring(0, maxLength - endTextIfTooLong.length)}${endTextIfTooLong}` : text;
 }
 
 function getKeyByValue<O extends {[key: string]: any}>(object: O, value: O[keyof O]): keyof O | undefined {
 	return Object.keys(object).find(key => object[key] === value);
 }
 
-function cutIfTooLong(text: string, maxLength: number, endTextIfTooLong: string = '...'): string {
-	return text.length > maxLength ? `${text.substring(0, maxLength - endTextIfTooLong.length)}${endTextIfTooLong}` : text;
+function random<T extends any>(array: T[]): T {
+	return array[Math.floor(Math.random() * array.length)];
+}
+
+module.exports = {
+	cutIfTooLong,
+	getKeyByValue,
+	random,
 }
