5b2450b1d22abba42d83a1c0534341c519084c97
Ayfri <pierre.ayfri@gmail.com>
2021-01-22 00:54:35 +0100

    Fixed export, I'm tired haha.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,25 +1,18 @@
 import {Snowflake} from 'discord.js';
 import CommandHandler from '../CommandHandler.js';
 
-function cutIfTooLong(text: string, maxLength: number, endTextIfTooLong: string = '...'): string {
+export function cutIfTooLong(text: string, maxLength: number, endTextIfTooLong: string = '...'): string {
 	return text.length > maxLength ? `${text.substring(0, maxLength - endTextIfTooLong.length)}${endTextIfTooLong}` : text;
 }
 
-function getKeyByValue<O extends {[key: string]: any}>(object: O, value: O[keyof O]): keyof O | undefined {
+export function getKeyByValue<O extends {[key: string]: any}>(object: O, value: O[keyof O]): keyof O | undefined {
 	return Object.keys(object).find(key => object[key] === value);
 }
 
-function random<T extends any>(array: T[]): T {
+export function random<T extends any>(array: T[]): T {
 	return array[Math.floor(Math.random() * array.length)];
 }
 
-function isOwner(id: Snowflake): boolean {
+export function isOwner(id: Snowflake): boolean {
 	return CommandHandler.owners?.includes(id);
-}
-
-module.exports = {
-	cutIfTooLong,
-	getKeyByValue,
-	isOwner,
-	random,
 }
