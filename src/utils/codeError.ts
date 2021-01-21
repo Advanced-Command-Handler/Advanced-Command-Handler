2a00110b05d6cd7b8f946ea399b214887931c8d8
Ayfri <pierre.ayfri@gmail.com>
2021-01-22 00:57:03 +0100

    Added codeError function.

---
+++ src/utils/codeError.ts
@@ -0,0 +1,19 @@
+import {Message} from 'discord.js';
+import {BetterEmbed} from 'discord.js-better-embed';
+import {Command} from '../classes/Command';
+import {Logger} from './Logger';
+import {isOwner} from './utils';
+
+export default (message: Message, error: Error, command: Command): Promise<Message> => {
+	const embed = BetterEmbed.fromTemplate('complete', {
+		client: message.client,
+		color: 0xee2200,
+		title: 'Code error :',
+		description: error.stack,
+	});
+
+	Logger.error(error, 'CodeError');
+	return isOwner(message.author.id)
+	       ? message.channel.send(embed)
+	       : message.channel.send(`An error occurred while executing the \`${command.name}\` command.`);
+};
