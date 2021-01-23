c34df4e0f9b657fe8c5e6c46d3ac9ff0498b943f
Ayfri <pierre.ayfri@gmail.com>
2021-01-23 17:37:06 +0100

    Fixed exports in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import dayjs from 'dayjs';
-import argError from './utils/argError';
-import permissionsError from './utils/permissionsError';
-export {argError, dayjs, permissionsError};
+export {dayjs};
 
+export * from 'discord.js-better-embed';
 export * from './CommandHandler';
 export * from './classes/Command';
 export * from './classes/AdvancedClient';
 export * from './classes/CommandHandlerError';
 export * from './classes/Event';
-export * from './utils/Logger';
+export * from './utils/argError';
 export * from './utils/getThing';
+export * from './utils/Logger';
+export * from './utils/permissionsError';
 export * from './utils/utils';
-export * from 'discord.js-better-embed';
