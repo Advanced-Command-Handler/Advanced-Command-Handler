0d24bafbdb5a1d9e1bd785e6de2646c5866416fa
Ayfri <pierre.ayfri@gmail.com>
2021-01-23 15:43:26 +0100

    Added export for AdvancedClient.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import dayjs from 'dayjs';
 import CommandHandler from './CommandHandler';
+import AdvancedClient from './classes/AdvancedClient';
 import CommandHandlerError from './classes/CommandHandlerError';
 import Event from './classes/Event';
 import argError from './utils/argError';
 import permissionsError from './utils/permissionsError';
 
-export {CommandHandler, CommandHandlerError, Event, argError, dayjs, permissionsError};
+export {AdvancedClient, CommandHandler, CommandHandlerError, Event, argError, dayjs, permissionsError};
 export * from './classes/Command';
 export * from './utils/Logger';
 export * from './utils/getThing';
