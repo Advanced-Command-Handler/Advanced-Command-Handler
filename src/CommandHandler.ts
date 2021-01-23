5cbf07b48214824208f638798747e7deeab794f3
Ayfri <pierre.ayfri@gmail.com>
2021-01-23 17:25:34 +0100

    Added types for CommandHandler.

--- src/CommandHandler.ts
+++ src/CommandHandler.ts
@@ -1,3 +1,7 @@
+/**
+ * @export CommandHandler
+ */
+
 import {ClientOptions, Collection, Message, Snowflake} from 'discord.js';
 import {EventEmitter} from 'events';
 import {promises as fsPromises} from 'fs';
@@ -45,14 +49,31 @@ namespace CommandHandler {
 	export let prefixes: string[] = [];
 	export let client: AdvancedClient | null = null;
 
+	/**
+	 * @typeParam K - Events names for CommandHandler.
+	 * @param eventName - The event name.
+	 * @param fn - The callback to execute.
+	 */
 	export function on<K extends keyof CommandHandlerEvents>(eventName: K, fn: (listener: CommandHandlerEvents[K]) => void): void {
 		emitter.on(eventName, fn);
 	}
 
+	/**
+	 * @param eventName - The event name.
+	 * @param args - The arguments to pass.
+	 */
 	export function emit<K extends keyof CommandHandlerEvents>(eventName: K, ...args: CommandHandlerEvents[K]) {
 		emitter.emit(eventName, args);
 	}
 
+	/**
+	 * Add the defaults events to your CommandHandler.
+	 *
+	 * @remarks
+	 * Must use after {@link CommandHandler.create}.
+	 *
+	 * @returns Itself.
+	 */
 	export function setDefaultEvents(): typeof CommandHandler {
 		Logger.info('Loading default events.', 'Loading');
 		for (let event of Object.values(defaultEvents)) {
@@ -64,6 +85,14 @@ namespace CommandHandler {
 		return CommandHandler;
 	}
 
+	/**
+	 * Add the defaults commands to your CommandHandler.
+	 *
+	 * @remarks
+	 * Must use after {@link CommandHandler.create}.
+	 *
+	 * @returns Itself.
+	 */
 	export function setDefaultCommands(): typeof CommandHandler {
 		Logger.info('Loading default commands.', 'Loading');
 		for (let command of Object.values(defaultCommands)) {
@@ -75,6 +104,12 @@ namespace CommandHandler {
 		return CommandHandler;
 	}
 
+	/**
+	 * Creates a new CommandHandler, wrap up the last one.
+	 *
+	 * @param options - Options for creating a new CommandHandler.
+	 * @returns Itself.
+	 */
 	export function create(options: CreateCommandHandlerOptions): typeof CommandHandler {
 		Logger.log(`Advanced Command Handler ${version} by Ayfri.`, 'Loading', 'red');
 		commandsDir = options.commandsDir;
@@ -88,6 +123,15 @@ namespace CommandHandler {
 		return CommandHandler;
 	}
 
+	/**
+	 * Launches the CommandHandler, log in the client and load commands/events.
+	 *
+	 * @param options - Options for launching the CommandHandler.
+	 * @param options.token - The token of your bot.
+	 * @param options.clientOptions - The client options, see {@link https://discord.js.org/#/docs/main/stable/typedef/ClientOptions}.
+	 *
+	 * @returns Itself in a promise.
+	 */
 	export async function launch(options: {token: string; clientOptions?: ClientOptions}): Promise<typeof CommandHandler> {
 		client = new AdvancedClient(options.token, options.clientOptions ?? {});
 		emit('launch');
@@ -114,10 +158,22 @@ namespace CommandHandler {
 		return CommandHandler;
 	}
 
+	/**
+	 * Get the prefix from the prefixes defined in {@link CommandHandler.launch} or null.
+	 *
+	 * @param message - The message to get the prefix for.
+	 * @returns The prefix found or null if not.
+	 */
 	export function getPrefixFromMessage(message: Message): string | null {
 		return prefixes.find(prefix => message.content.startsWith(prefix)) ?? null;
 	}
 
+	/**
+	 * Load a command from the directory & filename.
+	 *
+	 * @param path - The directory to get the command from.
+	 * @param name - The filename of the command.
+	 */
 	export async function loadCommand(path: string, name: string) {
 		let command: Command | (Command & {default: Command}) = await import(join(process.cwd(), `./${path}/${name}`));
 		if ('default' in command) command = command.default;
@@ -135,6 +191,14 @@ namespace CommandHandler {
 		Logger.comment(`Loading the command : ${Logger.setColor('gold', name)}`, 'Loading');
 	}
 
+	/**
+	 * Load all the commands from a directory.
+	 *
+	 * @remarks
+	 * The path must be a directory containing sub-directories.
+	 *
+	 * @param path - The path of the directory to load the commands from.
+	 */
 	export async function loadCommands(path: string) {
 		if (!path) return;
 		const dirs = await fsPromises.readdir(path);
@@ -154,6 +218,11 @@ namespace CommandHandler {
 		Logger.info(`${commands.size} commands loaded.`, 'Loading');
 	}
 
+	/**
+	 * Load all the events from a directory.
+	 *
+	 * @param path - The path of the directory to load the events from.
+	 */
 	export async function loadEvents(path: string) {
 		if (!path) return;
 		const files = await fsPromises.readdir(path);
@@ -172,6 +241,13 @@ namespace CommandHandler {
 		}
 	}
 
+	/**
+	 * Load an event.
+	 *
+	 * @param event - The event to load.
+	 *
+	 * @returns The event loaded.
+	 */
 	export function loadEvent(event: Event | (Event & {default: Event})): Event {
 		if ('default' in event) event = event.default;
 
