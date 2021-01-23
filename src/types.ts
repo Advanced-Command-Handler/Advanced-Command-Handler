85daabd9598e0697e74a102344aee2dcac60d51d
Ayfri <pierre.ayfri@gmail.com>
2021-01-23 17:25:14 +0100

    Added doc for types.ts.

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
 import {Message} from 'discord.js';
 import CommandHandler from './CommandHandler';
 
+/**
+ * The runFunction to use for commands.
+ */
 export type RunFunction = (...options: any[]) => Promise<any>;
 
+/**
+ * The runFunction to use when using the default message event.
+ */
 export type DefaultCommandRunFunction = (commandHandler: typeof CommandHandler, message: Message, args: string[]) => Promise<void>;
