0cedaf3324825efa737c8efecbe4cfda653d3fc8
Ayfri <pierre.ayfri@gmail.com>
2021-05-26 04:32:01 +0200

    feat: Update tests to new command syntax.

--- tests/commands/tests/say.js
+++ tests/commands/tests/say.js
@@ -1,15 +1,10 @@
 const {Command} = require('advanced-command-handler');
 
 module.exports = class SayCommand extends Command {
-	constructor() {
-		super({
-				name:            'say',
-				tags:            ['guildOnly'],
-				userPermissions: ['MANAGE_MESSAGES'],
-				cooldown:        10,
-			}
-		);
-	}
+	name = 'say';
+	tags = ['guildOnly'];
+	userPermissions = ['MANAGE_MESSAGES'];
+	cooldown = 10;
 
 	async run(ctx) {
 		ctx.message.channel.send(ctx.args.length ? ctx.argString : 'You must specify a message.');
--- tests/commands/tests/tests.js
+++ tests/commands/tests/tests.js
@@ -1,14 +1,9 @@
 const {Command} = require('advanced-command-handler');
 module.exports = class TestCommand extends Command {
-	constructor() {
-		super({
-			name:            'tests',
-			aliases:         ['test', 't'],
-			channels:        [],
-			tags:            ['nsfw'],
-			userPermissions: ['MANAGE_MESSAGES']
-		});
-	}
+	name = 'tests';
+	aliases = ['test', 't'];
+	tags = ['nsfw'];
+	userPermissions = ['MANAGE_MESSAGES'];
 
 	async run(ctx) {
 		ctx.message.channel.send('testing');
