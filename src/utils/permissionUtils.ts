38dfabaebc0a5b25ec8f3e3bcdb45354b71e8b76
Ayfri <pierre.ayfri@gmail.com>
2021-06-29 05:24:54 +0200

    refactor: Remove hasPermission function.

--- src/utils/permissionUtils.ts
+++ src/utils/permissionUtils.ts
@@ -32,19 +32,3 @@ export function permissionsError(message: Message, missingPermissions: Permissio
 export function isPermission(permission: string): permission is PermissionString {
 	return Object.keys(Permissions.FLAGS).includes(permission);
 }
-
-/**
- * Tells you if the client has permissions from a message in a fancier way than the {@link https://discord.js.org/#/docs/main/stable/class/GuildMember?scrollTo=hasPermission | GuildMember#hasPermission} method.
- *
- * @param message - The message to check permissions from.
- * @param permission - The permission to check.
- * @returns - If the user has the permission.
- */
-function hasPermission(message: Message, permission: PermissionResolvable) {
-	return message.guild
-		? message.guild.me?.hasPermission(permission, {
-				checkOwner: false,
-				checkAdmin: false,
-		  })
-		: false;
-}
