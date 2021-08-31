8398b53e1effcaaa15bb66894ea276585a96161e
Ayfri <pierre.ayfri@gmail.com>
2021-08-31 06:13:51 +0200

    feat: Add MaybePromise type util.

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,4 @@ export type Constructor<T extends {} = {}> = new (...args: any[]) => T;
 export type MaybeCommand = Constructor<Command> | {default: Constructor<Command>} | {[k: string]: Constructor<Command>};
 export type MaybeEvent = Constructor<Event> | {default: Constructor<Event>} | {[k: string]: Constructor<Event>};
 export type TextChannelLike = TextChannel | NewsChannel;
+export type MaybePromise<T> = Promise<T> | T;
